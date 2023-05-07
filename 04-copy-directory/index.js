1724c2ad7c72c64e5b8ccb2a0b695084a637e7d7
Artem Baluev <active.oil5287@fastmail.com>
2023-05-07 22:28:26 +0500

    feat: complete 04-copy-directory subtask

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs');
+const promises = require('fs/promises');
+const path = require('path');
 
+const filesDir = path.join(__dirname, 'files');
+const filesCopyDir = path.join(__dirname, 'files-copy');
+
+
+async function copyDir(){
+  await promises.rm(filesCopyDir, { recursive:true, force: true});
+  promises.mkdir(filesCopyDir, { recursive:true });
+  fs.readdir(filesDir, { withFileTypes: true }, (err, files) => {
+    files.forEach(file => {
+      if(! file.isDirectory()){
+        promises.copyFile(
+          path.join(filesDir, file['name']),
+          path.join(filesCopyDir, file['name'])
+        );
+      }
+    })
+  });
+}
+copyDir();
