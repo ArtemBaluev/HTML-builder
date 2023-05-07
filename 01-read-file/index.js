5800abf16f8dfe6fc2c367812a214fad4eb72f26
Artem Baluev <active.oil5287@fastmail.com>
2023-05-07 11:59:15 +0500

    feat: complete 01-read-file subtask

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,2 +1,13 @@
+const fs = require('fs');
+const path = require('path');
 
+const { stdout } = process;
 
+let file = path.join(__dirname, 'text.txt');
+
+const stream = fs.createReadStream(file, 'utf-8');
+
+let data = '';
+
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => stdout.write(data));
